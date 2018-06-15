70ed2ae06ff38806f93f4a9cf624ad1b84eaf4f6
LevelMeUp <samueldavidader@gmail.com>
2018-06-15 21:17:45 +0200

    My first app, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ader sam\documents\010 programming and software\060 websites\080 roam_django\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
